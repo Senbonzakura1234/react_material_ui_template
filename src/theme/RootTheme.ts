ced609ce177e2aeb119f65dd6486d606a54c48a7
Senbonzakura <anhdungpham090@gmail.com>
2020-12-08 09:56:50 +0700

    Fix theme primary dark.

--- src/theme/RootTheme.ts
+++ src/theme/RootTheme.ts
@@ -3,41 +3,41 @@ import { createMuiTheme } from "@material-ui/core";
 const RootTheme = createMuiTheme({
     palette: {
         primary: {
-            light: '#6a1b9a',
-            main: '#6a1b9a',
-            dark: '#Dark',
-            contrastText: '#ffffff',
+            light: "#9c4dcc",
+            main: "#6a1b9a",
+            dark: "#38006b",
+            contrastText: "#ffffff"
         },
         secondary: {
-            light: '#ffc4ff',
-            main: '#ce93d8',
-            dark: '#9c64a6',
-            contrastText: '#000000',
+            light: "#ffc4ff",
+            main: "#ce93d8",
+            dark: "#9c64a6",
+            contrastText: "#000000"
         },
         success: {
-            light: '#5efc82',
-            main: '#00c853',
-            dark: '#009624',
-            contrastText: '#ffffff',
+            light: "#5efc82",
+            main: "#00c853",
+            dark: "#009624",
+            contrastText: "#ffffff"
         },
         info: {
-            light: '#73e8ff',
-            main: '#29b6f6',
-            dark: '#0086c3',
-            contrastText: '#ffffff',
+            light: "#73e8ff",
+            main: "#29b6f6",
+            dark: "#0086c3",
+            contrastText: "#ffffff"
         },
         error: {
-            light: '#f05545',
-            main: '#b71c1c',
-            dark: '#7f0000',
-            contrastText: '#ffffff',
+            light: "#f05545",
+            main: "#b71c1c",
+            dark: "#7f0000",
+            contrastText: "#ffffff"
         },
         warning: {
-            light: '#ffd149',
-            main: '#ffa000',
-            dark: '#c67100',
-            contrastText: '#000000',
-        },
+            light: "#ffd149",
+            main: "#ffa000",
+            dark: "#c67100",
+            contrastText: "#000000"
+        }
     },
 });
 
